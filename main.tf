79d178b5288c50c34785b1032a923e68aee5bc4f
joaquin610 <joaquinbanchero610@hotmail.com>
2024-07-06 23:47:29 -0300

    chore: Update AWS region in deployment workflow and Terraform configuration

--- main.tf
+++ main.tf
@@ -95,12 +95,15 @@ terraform {
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_session_token" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
 
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   token      = var.aws_session_token
-  region    = "us-east-1"
+  region     = var.aws_region
 }
 
 # VPC
