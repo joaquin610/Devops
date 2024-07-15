1026c162ae60d54a0000f24351f5ce00658a2f16
joaquin610 <joaquinbanchero610@hotmail.com>
2024-07-14 21:56:37 -0300

    chore: Update main.tf to include AWS ALB and ECS Service changes

--- main.tf
+++ main.tf
@@ -37,6 +37,29 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
 # Subnet 1
 resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.main.id
@@ -55,6 +78,17 @@ resource "aws_subnet" "subnet2" {
   tags = {
     Name = "Subnet 2"
   }
+}
+
+# Associate Route Table with Subnets
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Security Group
@@ -135,7 +169,7 @@ resource "aws_lb_listener" "http" {
 
 # ALB Target Group
 resource "aws_lb_target_group" "app" {
-  name     = "ecs-tg"
+  name     = "ecs-tg"  # Cambiar el nombre para evitar conflicto
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
